83bbb5b8842dbac8f0deaa2b252495b6ffdc1101
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-06-11 09:47:52 -0400

    Finished this Explicit Binding

--- assignments/this.js
+++ assignments/this.js
@@ -2,9 +2,12 @@
 * in your own words. explain the four principle for the "this" keyword below.
 *
 * 1. window Binding -
+
 * 2. Implicit Binding - When you call a function or when it is invoked, to the left of the dot is what what 'this' is referencing.
+
 * 3. new Binding -
-* 4. Explicit Binding -
+
+* 4. Explicit Binding - When you call a funtion you explicitly state what the this keyword is going to reference.
 *
 * write out a code example of each explanation above
 */
@@ -29,3 +32,13 @@ me.sayName();
 
 // Principle 4
 // code example for Explicit Binding
+var sayName = function() {
+    console.log(`My name is ${this.name}`);
+};
+
+var me = {
+    name: 'Isaiah',
+    age: 18,
+};
+
+sayName.call(me);
