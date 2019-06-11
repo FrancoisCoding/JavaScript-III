4e3e94369f48d85239eeca9881f9fbceae7ae2d7
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-06-11 09:39:49 -0400

    Finished this Implicit Binding

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,31 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. window Binding -
+* 2. Implicit Binding - When you call a function or when it is invoked, to the left of the dot is what what 'this' is referencing.
+* 3. new Binding -
+* 4. Explicit Binding -
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
-
 // code example for Window Binding
 
 // Principle 2
-
 // code example for Implicit Binding
 
-// Principle 3
+var me = {
+    name: 'Isaiah',
+    age: 18,
+    sayName: function() {
+        console.log(this.name)
+    }
+}
+me.sayName();
 
+// Principle 3
 // code example for New Binding
 
 // Principle 4
-
 // code example for Explicit Binding
